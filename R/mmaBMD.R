97d8a96e821802c73a70494fe2c4e3a2ce4c56a2
daniel-gerhard <gerhard@biostat.uni-hannover.de>
2013-08-23 11:28:44 +0200

    separated mmaED into mmaED and mmaBMD and extended the functions to several curveids

---
+++ R/mmaBMD.R
@@ -0,0 +1,116 @@
+
+mmaBMD <- function(object, ..., respLev, ic = "AIC", interval = c("none", "buckland", "kang"), level = 0.95, bmd = c("additional", "extra"), background = 0.05){
+  lllist <- list(object, ...)
+  ismedrc <- sapply(lllist, function(x) inherits(x, "medrc") | inherits(x, "glsdrc"))
+  mllist <- lllist[ismedrc]  
+  Call <- match.call()
+  Call$respLev <- NULL
+  Call$ic <- NULL
+  Call$interval <- NULL
+  Call$level <- NULL
+  Call$background <- NULL
+  Call$bmd <- NULL
+  Call$dmlist <- NULL
+  mnames <- as.character(Call[-1L])[ismedrc]
+  # number of curves
+  if (any(ncol(mllist[[1]]$parmMat) != sapply(mllist, function(x) ncol(x$parmMat)))) stop("Number of curves are not the same for all models!")
+  # model weights
+  if (ic == "AIC") inc <- sapply(mllist, AIC)
+  if (ic == "BIC") inc <- sapply(mllist, BIC)
+  d <- inc-min(inc)
+  wi <- exp(-0.5*d)/sum(exp(-0.5*d))
+  names(wi) <- mnames  
+  
+  # adjusting respLev for BMD
+  if (bmd[1] == "extra") respLev <- respLev * (1-background)  
+  lenRL <- length(respLev)
+  respLevMat <- lapply(1:lenRL, function(i) sapply(1:length(mllist),function(x){
+    objectFit <- mllist[[x]]
+    cobj <- objectFit$parmMat
+    prnames <- rownames(cobj)
+    cvals <- cobj[prnames == "c", ]
+    if (length(cvals) == 0) {cvals <- 0}  # setting c if missing (not estimated)
+    dvals <- cobj[prnames == "d", ]
+    if (length(cvals) == 1 & length(dvals) > 1) cvals <- rep(cvals, length(dvals))
+    if (length(dvals) == 1 & length(cvals) > 1) dvals <- rep(dvals, length(cvals))
+    if (any(cvals > dvals)) {
+      tempd <- apply(cbind(cvals, dvals), 1, function(x) sort(x))
+      cvals <- tempd[1,]
+      dvals <- tempd[2,]
+    }
+    if (class(objectFit)[1] == "medrc"){
+      varcorr <- VarCorr(objectFit)
+      return(100 * (qnorm(1 - background) - qnorm(1-(background+respLev[i]/100))) * as.numeric(varcorr[attr(varcorr, "dimnames")[[1]] %in% "Residual", 2]) / (dvals - cvals))
+    }
+    if (class(objectFit)[1] == "glsdrc"){
+      return(100 * (qnorm(1 - background) - qnorm(1-(background+respLev[i]/100))) * objectFit$fit$sigma / (dvals - cvals))     
+    }
+  }))
+  
+  # ED estimation
+  edl <- lapply(1:length(mllist), function(i){
+    mobj <- mllist[[i]]
+    parmMat <- mobj$parmMat
+    if (ncol(parmMat) > 1){
+      lapply(1:length(respLev), function(rl) sapply(1:ncol(parmMat), function(j)  ED(mobj, respLev=respLevMat[[rl]][j,i], clevel=colnames(parmMat)[j], display=FALSE)))
+    } else {
+      lapply(1:length(respLev), function(rl) t(ED(mobj, respLev=respLevMat[[rl]][i], display=FALSE)))    
+    }    
+  })
+  edm <- sapply(1:length(edl), function(i) sapply(edl[[i]], function(x) x[1,]))
+  edse <- sapply(1:length(edl), function(i) sapply(edl[[i]], function(x) x[2,]))
+  
+  # model-averaged ED
+  edma <- apply(t(t(edm) * wi), 1, sum)
+  
+  # rownames
+  parmMat <- mllist[[1]]$parmMat
+  if (ncol(parmMat) > 1){
+    rcnames <- paste(colnames(parmMat), rep(respLev, each=ncol(parmMat)), sep=":")
+  } else {
+    rcnames <- as.character(respLev)
+  }
+  
+  if (interval[1] == "none"){
+    retMat <- cbind("Estimate" = edma)
+  }  
+  
+  if (interval[1] == "buckland"){
+    sema <- apply(sqrt(edse^2 + (edm - apply(edm, 1, mean))^2), 1, function(x) sum(x*wi))
+    quant <- qnorm(1 - (1 - level)/2) * sema
+    retMat <- as.matrix(cbind(edma, sema, edma - quant, edma + quant))
+    colnames(retMat) <- c("Estimate", "SE", "Lower", "Upper")
+  }
+  
+  if (interval[1] == "kang"){
+    edlci <- lapply(1:length(mllist), function(i){
+      mobj <- mllist[[i]]
+      parmMat <- mobj$parmMat
+      if (ncol(parmMat) > 1){
+        lapply(1:length(respLev), function(rl) sapply(1:ncol(parmMat), function(j)  ED(mobj, respLev=respLevMat[[rl]][j,i], clevel=colnames(parmMat)[j], display=FALSE, interval="delta")))
+      } else {
+        lapply(1:length(respLev), function(rl) t(ED(mobj, respLev=respLevMat[[rl]][i], display=FALSE, interval="delta")))    
+      }    
+    })
+    ll <- sapply(1:length(edlci), function(i) sapply(edlci[[i]], function(x) x[3,]))
+    ul <- sapply(1:length(edlci), function(i) sapply(edlci[[i]], function(x) x[4,]))
+    retMat <- as.matrix(cbind(edma, apply(ll, 1, function(x) sum(x*wi)), apply(ul, 1, function(x) sum(x*wi))))
+    colnames(retMat) <- c("Estimate", "Lower", "Upper")
+  }  
+  
+  rownames(retMat) <- rcnames
+  rownames(edm) <- rcnames
+  colnames(edm) <- mnames
+  rlm <- sapply(1:length(mllist), function(i) sapply(respLevMat, function(x) rbind(x)[,i]))
+  rownames(rlm) <- rcnames
+  colnames(rlm) <- mnames
+  
+  out <- list()
+  out$effectiveRespLev <- rlm
+  out$retMat <- retMat
+  out$weights <- wi
+  out$EDi <- edm
+  if (interval[1] == "buckland") {out$SEi <- edse}  
+  return(out)  
+  
+}
--- R/mmaED.R
+++ R/mmaED.R
@@ -1,82 +1,56 @@
-mmaED <-
-function(object, ..., respLev, ic = "AIC", interval = c("none", "buckland", "kang"), level = 0.9, bmd = c("none", "additional", "extra"), 
-background = 0.05, dmList = NULL){
+mmaED <- function(object, ..., respLev, ic = "AIC", interval = c("none", "buckland", "kang"), level = 0.95){
   lllist <- list(object, ...)
   ismedrc <- sapply(lllist, function(x) inherits(x, "medrc") | inherits(x, "glsdrc"))
   mllist <- lllist[ismedrc]  
-  msl <- sapply(mllist, function(x) x$mselect)
   Call <- match.call()
   Call$respLev <- NULL
   Call$ic <- NULL
   Call$interval <- NULL
   Call$level <- NULL
-  Call$bmd <- NULL
-  Call$background <- NULL
-  Call$dmList <- NULL
-  colnames(msl) <- as.character(Call[-1L])[ismedrc]
-  aic <- t(msl)[,ic]
-  d <- aic-min(aic)
+  mnames <- as.character(Call[-1L])[ismedrc]
+  # number of curves
+  if (any(ncol(mllist[[1]]$parmMat) != sapply(mllist, function(x) ncol(x$parmMat)))) stop("Number of curves are not the same for all models!")
+  # model weights
+  if (ic == "AIC") inc <- sapply(mllist, AIC)
+  if (ic == "BIC") inc <- sapply(mllist, BIC)
+  d <- inc-min(inc)
   wi <- exp(-0.5*d)/sum(exp(-0.5*d))
-  names(wi) <- colnames(msl)
-
-  if (bmd[1] == "extra") respLev <- respLev * (1-background)  
-  if (bmd[1] == "none"){
-    respLevMat <- matrix(respLev, nrow=length(lllist), ncol=length(respLev), byrow=TRUE)    
-  } else {
-    lenRL <- length(respLev)
-    respLevMat <- matrix(sapply(1:lenRL, function(i) sapply(1:length(lllist),function(x){
-      objectFit <- lllist[[x]]$fit
-      cVal <- try(fixef(objectFit)[["c"]], silent = TRUE)
-      if (inherits(cVal, "try-error")) {cVal <- 0}  # setting c if missing (not estimated)
-      dVal <- fixef(objectFit)[["d"]]
-      if (cVal > dVal) {tempVal <- cVal; cVal <- dVal; dVal <- tempVal}
-      varcorr <- VarCorr(objectFit)
-      100 * (qnorm(1 - background) - qnorm(1-(background+respLev[i]/100))) * as.numeric(varcorr[attr(varcorr, "dimnames")[[1]] %in% "Residual", 2]) / (dVal - cVal)
-    })), ncol=lenRL)
-  }
-  ## Calculating ED values
-  if (is.null(dmList) || interval[1] == "kang"){
-    edEst <- t(sapply(1:length(mllist), function(x) ED(mllist[[x]], respLevMat[x,], display = FALSE)[,1]))  
-    if (nrow(respLevMat) != nrow(edEst)) edEst <- t(edEst)
-  } else {
-    edESSE <- lapply(1:ncol(respLevMat), function(i) sapply(1:length(dmList), function(x){deltaMethod(mllist[[x]]$fit, dmList[[x]](respLevMat[x,i]/100), vcov(mllist[[x]]$fit))}))
-    edEst <- matrix(sapply(edESSE, function(x) as.numeric(x[1, , drop = FALSE])), nrow=length(dmList), ncol=length(respLev))
-    edSe <- matrix(sapply(edESSE, function(x) as.numeric(x[2, , drop = FALSE])), nrow=length(dmList), ncol=length(respLev))
-  }
-  rownames(edEst) <- names(wi)
-  edVec <- apply(edEst * wi, 2, sum)
-
+  names(wi) <- mnames
+  
+  # calculate ED per model
+  edlist <- lapply(mllist, function(x) ED(x, respLev=respLev, display=FALSE))
+  edm <- rbind(sapply(edlist, function(x) x[,1]))
+  colnames(edm) <- mnames
+  
+  # model-averaged ED
+  edma <- apply(t(t(edm) * wi), 1, sum)
+  
   if (interval[1] == "none"){
-    retMat <- cbind("Estimate" = edVec)
+    retMat <- cbind("Estimate" = edma)
   }  
+  
   if (interval[1] == "buckland"){
-    if (is.null(dmList)){
-      edSe <- t(sapply(1:length(mllist), function(x) ED(mllist[[x]], respLevMat[x,], display = FALSE)[,2]))
-      if (ncol(respLevMat) == 1) edSe <- t(edSe)     
-    }
-    seVec <- apply(sqrt(edSe^2 + (t(t(edEst) - apply(edEst, 2, mean)))^2) * wi, 2, sum)
-    quantVal <- qnorm(1 - (1 - level)/2) * seVec
-    retMat <- as.matrix(cbind(edVec, seVec, edVec - quantVal, edVec + quantVal))
+    edse <- rbind(sapply(edlist, function(x) x[,2]))
+    sema <- apply(sqrt(edse^2 + (edm - apply(edm, 1, mean))^2), 1, function(x) sum(x*wi))
+    quant <- qnorm(1 - (1 - level)/2) * sema
+    retMat <- as.matrix(cbind(edma, sema, edma - quant, edma + quant))
     colnames(retMat) <- c("Estimate", "SE", "Lower", "Upper")
   }
-
+  
   if (interval[1] == "kang"){
-    edCll <- t(sapply(1:length(mllist), function(x) ED(mllist[[x]], respLevMat[x,], interval="delta", level = level, display = FALSE)[,3]))
-    edClu <- t(sapply(1:length(mllist), function(x) ED(mllist[[x]], respLevMat[x,], interval="delta", level = level, display = FALSE)[,4]))
-    if (ncol(respLevMat) == 1){
-      edCll <- t(edCll)
-      edClu <- t(edClu)
-    }
-    retMat <- as.matrix(cbind(edVec, apply(edCll * wi, 2, sum), apply(edClu * wi, 2, sum)))
+    edclist <- lapply(mllist, function(x) ED(x, respLev=respLev, interval="delta", display=FALSE))
+    ll <- rbind(sapply(edclist, function(x) x[,3]))
+    ul <- rbind(sapply(edclist, function(x) x[,4]))
+    retMat <- as.matrix(cbind(edma, apply(ll, 1, function(x) sum(x*wi)), apply(ul, 1, function(x) sum(x*wi))))
     colnames(retMat) <- c("Estimate", "Lower", "Upper")
   }  
+  
   out <- list()
-  out$effectiveRespLev <- if (bmd[1] == "none") respLev else respLevMat
+  out$effectiveRespLev <- respLev
   out$retMat <- retMat
   out$weights <- wi
-  out$EDi <- edEst
-  if (interval[1] == "buckland") {out$SEi <- edSe}  
+  out$EDi <- edm
+  if (interval[1] == "buckland") {out$SEi <- edse}  
   return(out)
 }
-
 
