49b024c9f6f299b4da6e898d7c50e110a377e75e
daniel-gerhard <gerhard@biostat.uni-hannover.de>
2013-10-15 09:53:11 +0200

    changed fixed effect starting value search from nls to drm

--- R/glsdrm_helper.R
+++ R/glsdrm_helper.R
@@ -10,9 +10,9 @@ findfixedstartvalsglsdrm <- function(form, data, cid, fct, fid, mform){
     cform <- as.formula(paste(as.character(form)[2], as.character(form)[1], as.character(form)[3], "+", cid))
     mf <- model.frame(cform, data)
     spl <- split(mf, mf[,3])
-    fullp <- coefficients(eval(parse(text=paste("nls(", mform, ", data=mf[,1:2])", sep=""))))
+    fullp <- coefficients(drm(form, data=mf[,1:2], fct=fct))
     pmat <- t(sapply(spl, function(x){
-      trycoef <- try(coefficients(eval(parse(text=paste("nls(", mform, ", data=x)", sep="")))), silent=TRUE)  
+      trycoef <- try(coefficients(drm(form, data=x, fct=fct)), silent=TRUE)  
       if (class(trycoef)[1] == "try-error") return(fullp) else return(trycoef)
     }))    
     plist <- lapply(1:length(fid), function(i){
--- R/medrm_helper.R
+++ R/medrm_helper.R
@@ -1,18 +1,18 @@
 makehelpfunction <- function(fct){
-  drcfunction <- function(){
+  eval(parse(text="drcfunction <- function(){
     if (is.null(fct$fixed)) fct$fixed <- rep(NA, length(fct$names))
     parmVec <- fct$fixed
     notFixed <- is.na(parmVec)
     numParm <- length(parmVec)
-    .value <- fct$fct(dose, eval(parse(text=paste("cbind(", paste(fct$names, collapse=", "),")", sep = ""))))
+    .value <- fct$fct(dose, eval(parse(text=paste('cbind(', paste(fct$names, collapse=', '),')', sep = ''))))
     .actualArgs <- as.list(match.call()[fct$names])
     if (all(unlist(lapply(.actualArgs, is.name)))) {
-      .grad <- fct$deriv1(dose, eval(parse(text=paste("cbind(", paste(fct$names, collapse=", "),")", sep = ""))))
+      .grad <- fct$deriv1(dose, eval(parse(text=paste('cbind(', paste(fct$names, collapse=', '),')', sep = ''))))
       dimnames(.grad) <- list(NULL, .actualArgs)
-      attr(.value, "gradient") <- .grad
+      attr(.value, 'gradient') <- .grad
     }
     .value
-  }
+  }"))
   aargs <- paste("dose=,", paste(fct$names, collapse="=,"), "=", sep="")
   formals(drcfunction) <- eval(parse(text = paste("alist(", aargs,")", sep = "")))
   attr(drcfunction, "pnames") <- fct$names
@@ -36,11 +36,11 @@ findfixedstartvals <- function(form, data, cid, fct, fid, mform){
     cform <- as.formula(paste(as.character(form)[2], as.character(form)[1], as.character(form)[3], "+", cid))
     mf <- model.frame(cform, data)
     spl <- split(mf, mf[,3])
-    fullp <- coefficients(eval(parse(text=paste("nls(", mform, ", data=mf[,1:2])", sep=""))))
+    fullp <- coefficients(drm(form, data=mf[,1:2], fct=fct))
     pmat <- t(sapply(spl, function(x){
-      trycoef <- try(coefficients(eval(parse(text=paste("nls(", mform, ", data=x)", sep="")))), silent=TRUE)  
+      trycoef <- try(coefficients(drm(form, data=x, fct=fct)), silent=TRUE)  
       if (class(trycoef)[1] == "try-error") return(fullp) else return(trycoef)
-    }))   
+    }))     
     plist <- lapply(1:length(fid), function(i){
       if (fid[i]) fullp[i] else pmat[,i]   
     }) 
