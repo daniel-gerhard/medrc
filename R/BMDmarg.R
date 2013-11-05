1908c5e91b084f3a34e761427d1b9f4f279727bf
daniel-gerhard <gerhard@biostat.uni-hannover.de>
2013-11-05 15:24:19 +0100

    added BMD and BMDmarg functions

---
+++ R/BMDmarg.R
@@ -0,0 +1,36 @@
+BMDmarg <- function(object, respLev, interval = c("none", "delta", "fls", "tfls"), level = 0.95, bmd = c("additional", "extra"), background = 0.05, nGQ=5, rfinterval=c(0, 1000), display=TRUE){
+  interval <- match.arg(interval)
+  bmd <- match.arg(bmd)
+  # adjusting respLev for BMD
+  if (bmd[1] == "extra") respLev <- respLev * (1-background)  
+  lenRL <- length(respLev)
+  adjrespLev <- sapply(1:lenRL, function(i){ 
+    cobj <- object$parmMat
+    prnames <- rownames(cobj)
+    cvals <- cobj[prnames == "c", ]
+    if (length(cvals) == 0) {cvals <- 0}  # setting c if missing (not estimated)
+    dvals <- cobj[prnames == "d", ]
+    if (length(cvals) == 1 & length(dvals) > 1) cvals <- rep(cvals, length(dvals))
+    if (length(dvals) == 1 & length(cvals) > 1) dvals <- rep(dvals, length(cvals))
+    if (any(cvals > dvals)) {
+      tempd <- apply(cbind(cvals, dvals), 1, function(x) sort(x))
+      cvals <- tempd[1,]
+      dvals <- tempd[2,]
+    }
+    if (class(object)[1] == "medrc"){
+      varcorr <- VarCorr(object)
+      return(100 * (qnorm(1 - background) - qnorm(1-(background+respLev[i]/100))) * as.numeric(varcorr[attr(varcorr, "dimnames")[[1]] %in% "Residual", 2]) / (dvals - cvals))
+    }
+    if (class(object)[1] == "glsdrc"){
+      return(100 * (qnorm(1 - background) - qnorm(1-(background+respLev[i]/100))) * object$fit$sigma / (dvals - cvals))     
+    }
+  }) 
+  intLabel <- NULL
+  if (interval == "delta") intLabel <- "Delta method"
+  if (interval == "tfls") intLabel <- "To and from log scale"
+  if (interval == "fls") intLabel <- "Back-transformed from log scale"
+  bmdrn <- rownames(ED(object, respLev=respLev, display=FALSE))
+  bmdest <- EDmarg(object, respLev=adjrespLev, interval=interval, level=level, nGQ=nGQ, rfinterval=rfinterval, display=FALSE)
+  rownames(bmdest) <- bmdrn
+  eval(parse(text="drc:::resPrint(bmdest, 'Estimated benchmark doses', interval, intLabel, display = display)"))
+}
